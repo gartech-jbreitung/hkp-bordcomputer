b4c6c523f14548a8a6352fc9eb0b2acbc5e7e145
jonathanschlitt <development@jonathanschlitt.de>
2022-11-03 14:10:50 +0100

    first commit

---
+++ Autohersteller_01/src/USB.java
@@ -0,0 +1,90 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class USB implements Device {
+
+	public int volume = 0;
+
+	@Override
+	public void louder() {
+		System.out.println("Increasing Radio volume.");
+		this.volume++;
+	}
+
+	@Override
+	public void quieter() {
+		System.out.println("Decreasing Radio volume.");
+		this.volume--;
+	}
+
+	@Override
+	public int getVolume() {
+		return this.volume;
+	}
+
+	@Override
+	public void next() {
+		System.out.println("Switching to next radio station with higher frequency!");
+	}
+
+	@Override
+	public void prev() {
+		System.out.println("Switching to next radio station with higher frequency!");
+	}
+
+	@Override
+	public String getInfotext() {
+		return "I`m your Radio...";
+	}
+
+	@Override
+	public String[] getOptions() {
+
+		Method[] availableOptions = USB.class.getDeclaredMethods();
+		String[] options = new String[availableOptions.length];
+
+		for (int i = 0; i < availableOptions.length; i++) {
+			options[i] = availableOptions[i].getName();
+		}
+
+		return options;
+	}
+
+	@Override
+	public void chooseOption(String opt) {
+
+		Method[] methods = USB.class.getDeclaredMethods();
+
+		for (int i = 0; i < methods.length; i++) {
+			if (methods[i].getName().contains(opt)) {
+				// System.out.println(opt);
+				try {
+					methods[i].invoke(this, null);
+				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+					e.printStackTrace();
+				}
+			} else {
+				continue;
+			}
+		}
+
+	}
+
+	@Override
+	public String play() {
+		return "Radio is playing...";
+	}
+
+	public void changeRadioStation() {
+		System.out.println("You entered YouFM ==> Now playing YouFM :)");
+	}
+
+	public void changeRadioReceivingMethod() {
+		System.out.println("Switched from AM to FM...");
+	}
+
+	public void addStationToFavourites() {
+		System.out.println("This Radio Station was saved to your favourites.");
+	}
+
+}
